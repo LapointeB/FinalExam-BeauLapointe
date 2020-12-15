0d94a274681193f46be047e286d54360c104a419
LapointeB <LapointeB@T29685.niaccit.niacc.edu>
2020-12-15 08:52:58 -0600

    Finished the program

--- public_html/FinalExam.js
+++ public_html/FinalExam.js
@@ -4,17 +4,25 @@
  * and open the template in the editor.
  */
 let salesDays = [];
-let salesTotal;
+let salesTotal = 0;
 
-for (i = 1; i >= 7; i++) {
-    salesDays.push(Number(prompt("Enter your sales total for day " + i + ": ")));
+for (i = 1; i <= 7; i++) {
+    salesDays[i - 1] = prompt("Enter your sales total for day " + i + ": ");
+    salesDays[i - 1] = Number(salesDays[i - 1]);
 }
 
-for (i = 0; i > 7; i++) {
+for (i = 0; i < 7; i++) {
     salesTotal += salesDays[i];
 }
 
-let salesAverage = salesTotal / 7;
+let salesAverage = calcAve(salesTotal);
 
+salesDays.sort();
+let salesMax = salesDays[0];
+let salesMin = salesDays[6];
 
+document.write("Highest daily sales: " + salesMax + "<br>Lowest daily sales: " + salesMin + "<br>Average daily sales: " + salesAverage + "<br>Total weekly sales:" + salesTotal);
 
+function calcAve(total) {
+    return (total / 7);
+}
